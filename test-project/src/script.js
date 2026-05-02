f2d512367f0b9a637d5d7212c9665d014da39cc4
dibop <dibopieustanejosue@gmail.com>
2026-05-02 00:07:01 +0000

    déploiement terminé & last commit.

--- test-project/src/script.js
+++ test-project/src/script.js
@@ -1,4 +1,4 @@
-fetch('http://localhost:3000/api')
+fetch('https://node-deployment-test-project.onrender.com/api')
 .then(res => res.text())
 .then(data => document.getElementById('p').textContent = data)
 .catch(err => {document.getElementById('p').textContent = `erreur chargement : ${err}`})
