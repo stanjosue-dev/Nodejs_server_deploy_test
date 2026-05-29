b55f200158a35dfff073c1d214b9997c5b1d9b13
dibop <dibopieustanejosue@gmail.com>
2026-05-29 17:49:18 +0000

    API request restriction

--- server/app.js
+++ server/app.js
@@ -3,13 +3,27 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 
+
+const allowedOrigins = [
+    'http://127.0.0.1:5501',
+    'https://stanjosue-dev.github.io'
+];
+
 app.use(cors({
-    origin: [
-        'http://127.0.0.1:5500',
-        'https://stanjosue-dev.github.io'
-    ],
+    origin: allowedOrigins,
     optionsSuccessStatus: 200,
 }));
+
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if (origin && !allowedOrigins.includes(origin)) {
+        return res.status(403).json({ error: "Accès interdit : Origine non autorisée." });
+    }
+    if (!origin) {
+        return res.status(403).json({ error: "Accès interdit : Les requêtes directes ne sont pas autorisées." });
+    }
+    next();
+});
 
 
 app.get('/api', (req, res) => {
@@ -21,7 +35,6 @@ app.get('/api', (req, res) => {
     });
           
 })
-
 
 
 const PORT = process.env.PORT || 3000 ;
